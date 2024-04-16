be60e45cdb0517bca16b75717cff6d30dfde85c1
EunsuSeo01 <eunsu0613@naver.com>
2024-04-16 13:13:52 +0900

    Chore: 티빙 클론 프로젝트 초기 세팅 (#3)
    No storyboard 세팅

---
+++ Tving-Clone-Project/Tving-Clone-Project/ViewController/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  Tving-Clone-Project
+//
+//  Created by 서은수 on 4/16/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .red
+    }
+}
